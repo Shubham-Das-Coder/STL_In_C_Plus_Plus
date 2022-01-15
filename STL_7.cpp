b8a920c4d0c0cc1eeb9c0d634b04cd4e98e443a8
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2022-01-15 19:54:29 +0530

    Add files via upload

---
+++ STL_7.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+namespace shubham
+{
+    int val = 50;
+}
+
+int main()
+{
+    double val = 10.0;
+    std::cout << val << std::endl;
+    std::cout << shubham::val << std::endl;
+}
