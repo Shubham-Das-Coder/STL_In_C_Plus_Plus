f36e0677cc8e2fc6685410cdb094d855d561943f
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2022-01-15 23:19:07 +0530

    Add files via upload

---
+++ STL_Pairs_8.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    pair<int, string> p;
+    cin >> p.first;
+    cin >> p.second;
+    cout << p.first << endl;
+    cout << p.second << endl;
+}
