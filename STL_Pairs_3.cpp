33b22f075b2c353162e94a44c0069c427c5282c8
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2022-01-15 21:29:10 +0530

    Add files via upload

---
+++ STL_Pairs_3.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    pair<int, string> p;
+    p = {19, "Shubham"};
+    pair<int, string> p1 = p;
+    cout << p1.first << endl;
+    cout << p1.second << endl;
+}
