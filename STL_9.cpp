09faa7311e3ec771830fe896466376fd2dfeaab0
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2022-01-15 20:50:17 +0530

    Add files via upload

---
+++ STL_9.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct node
+{
+    string str;
+    int i;
+    double d;
+    char ch;
+};
+
+int main()
+{
+    node shubham;
+    shubham.str = "Shubham";
+    shubham.i = 19;
+    shubham.d = 97.5;
+    shubham.ch = 'S';
+    cout << shubham.str << endl;
+    cout << shubham.i << endl;
+    cout << shubham.d << endl;
+    cout << shubham.ch << endl;
+}
