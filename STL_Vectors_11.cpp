e4232c6a33e89e128bcc7113422f3ed36a102b0b
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2022-01-16 11:33:40 +0530

    Add files via upload

---
+++ STL_Vectors_11.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void printVector(vector<int> v)
+{
+    cout << "Size : " << v.size() << endl;
+    for (int i = 0; i < v.size(); ++i)
+    {
+        cout << v[i] << "  ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    vector<int> v;
+    v.push_back(10);
+    printVector(v);
+    v.pop_back();
+    printVector(v);
+}
