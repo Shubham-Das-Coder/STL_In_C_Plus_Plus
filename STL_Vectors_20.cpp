f47e0409051fbe2f3ee62fac932243997958ad40
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2022-01-16 15:32:09 +0530

    Add files via upload

---
+++ STL_Vectors_20.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void printVector(vector<string> &v)
+{
+    for (int i = 0; i < v.size(); ++i)
+        cout << v[i] << "  ";
+}
+
+int main()
+{
+    vector<string> v;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; ++i)
+    {
+        string s;
+        cin >> s;
+        v.push_back(s);
+    }
+    printVector(v);
+}
